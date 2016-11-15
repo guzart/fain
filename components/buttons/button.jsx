9bff96156690fe770127acab22d4ddb8502824cf
Arturo Guzman <arturo@guzart.com>
2016-11-14 22:39:10 -0500

    feat(button): add button styles and outline mode

--- components/buttons/button.jsx
+++ components/buttons/button.jsx
@@ -1,39 +1,57 @@
 import classNames from 'classnames';
+import omit from 'lodash/omit';
 import React, { PropTypes } from 'react';
+
+import { titleize } from '../../utils/inflectors';
 
 import styles from './button.scss';
 
-class Button extends React.PureComponent {
-  static propTypes = {
-    buttonStyle: PropTypes.oneOf(['default', 'primary']),
-    children: PropTypes.node,
-    className: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-    type: PropTypes.oneOf(['button', 'submit']),
-  };
+const buttonStyles = ['danger', 'info', 'primary', 'secondary', 'success', 'warning'];
 
-  static defaultProps = {
-    buttonStyle: 'default',
-    type: 'button',
-  };
-
-  render() {
-    const { buttonStyle, type, ...other } = this.props;
-    const buttonClassName = classNames(
-      this.props.className,
-      styles.btn,
-      styles[buttonStyle],
-    );
-
-    return (
-      <button
-        {...other}
-        className={buttonClassName}
-        type={type}
-      >
-        {this.props.children}
-      </button>
-    );
-  }
+function getButtonStyle(buttonStylesMap) {
+  return Object.keys(buttonStylesMap).find(k => styles[k]);
 }
+
+function Button(props) {
+  const { children, className, outline, type, ...other } = props;
+
+  const buttonProps = omit(other, buttonStyles);
+  const buttonStyle = getButtonStyle(other);
+  const buttonClassName = classNames(
+    className,
+    styles.button,
+    {
+      [styles[buttonStyle]]: !outline,
+      [styles[`outline${titleize(buttonStyle)}`]]: outline,
+    },
+  );
+
+  return (
+    <button
+      {...buttonProps}
+      className={buttonClassName}
+      type={type}
+    >
+      {children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  danger: PropTypes.bool,
+  info: PropTypes.bool,
+  outline: PropTypes.bool,
+  primary: PropTypes.bool,
+  secondary: PropTypes.bool,
+  success: PropTypes.bool,
+  warning: PropTypes.bool,
+  type: PropTypes.oneOf(['button', 'submit']),
+};
+
+Button.defaultProps = {
+  type: 'button',
+};
 
 export default Button;
---
+++ utils/inflectors.js
@@ -0,0 +1,5 @@
+
+export function titleize(value) {
+  const title = value.replace(/-/g, ' ');
+  return `${title[0].toUpperCase()}${title.substring(1)}`;
+}
