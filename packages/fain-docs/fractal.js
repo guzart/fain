12e700c1891204e974f80ba9009be4473d9e9654
Arturo Guzman <arturo@kisurfer.com>
2016-12-18 00:24:13 -0500

    chore(docs): moving docs to own package

--- packages/fain-docs/fractal.js
+++ packages/fain-docs/fractal.js
@@ -2,15 +2,17 @@ const fractal = require('@frctl/fractal').create(); // eslint-disable-line
 const path = require('path');
 const reactAdapter = require('./lib/frctl-react-adapter');
 
+const rootPath = path.resolve(__dirname, '..', '..');
+
 fractal.set('project.title', 'Fain Component Library');
 
 fractal.components.engine(reactAdapter);
-fractal.components.set('path', path.join(__dirname, '/src'));
+fractal.components.set('path', path.join(rootPath, 'src'));
 fractal.components.set('ext', '.react');
 fractal.components.set('default.display', {
   padding: '1rem',
 });
 
-fractal.docs.set('path', path.join(__dirname, '/docs'));
+fractal.docs.set('path', path.join(__dirname, 'docs'));
 
 module.exports = fractal;
