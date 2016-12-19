8411f61c32d0b906563ea64587e3389d168b8121
Arturo Guzman <arturo@kisurfer.com>
2016-12-18 22:03:24 -0500

    chore(log): fix debug message

--- packages/fain-src/lib/frctl-react-adapter/processSass.js
+++ packages/fain-src/lib/frctl-react-adapter/processSass.js
@@ -3,13 +3,12 @@ const sass = require('node-sass');
 const path = require('path');
 
 module.exports = function processSass(data, filename) {
-  debug(`proces sass ${filename}`);
+  debug(`procesSass: compile ${filename}`);
   const opts = {
     data: data,
     file: filename,
     includePaths: [path.resolve(__dirname, '..', '..', 'node_modules')]
   };
-  debug(`processSass Options: ${opts}`);
   const result = sass.renderSync(opts);
 
   return result.css.toString('utf8');
